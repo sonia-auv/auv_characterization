7c76a0a9c6b98dd8bfc9a5630736fa11f010e0b0
Martin Gauthier <gauthiermartin86@gmail.com>
2018-02-11 16:49:07 -0500

    added logger to function

--- Script/bag_to_image.py
+++ Script/bag_to_image.py
@@ -9,12 +9,30 @@
 import argparse
 import uuid
 import os
+import logging
+import sys
+import tqdm
 import cv2
 import rosbag
 from cv_bridge import CvBridge
 
+#TODO: CREATE DOC FOR THIS MODULES (COMMAND EXAMPLE)
+
 IMAGE_TOPIC_DEFAULT = '/provider_vision/Front_GigE/compressed'
 OUTPUT_FOLDER_DEFAULT = os.path.join(os.getcwd(), 'extraction')
+
+
+def set_logger(log_level=logging.INFO):
+    """Configure the logger with log_level."""
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+        level=log_level,
+        stream=sys.stderr)
+    logger = logging.getLogger("bag_to_image")
+    logging.getLogger('requests').setLevel(logging.WARNING)
+
+    return logger
+
 
 
 def parse_args():
@@ -41,7 +59,7 @@ def generate_uuid1_name():
     return str(uuid.uuid1())
 
 
-def main():
+def main(logger):
     """
     Extract a folder of images from a rosbag.
     """
@@ -53,28 +71,30 @@ def main():
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
 
+    bridge = CvBridge()
+
     for bag_file in bag_file_list:
+
 
         template_msg = 'Extract images from {} on topic {}'
         msg = template_msg.format(bag_file, args.image_topic)
-        print(msg)
+
+        logger.info(msg)
 
         bag_path = os.path.join(args.source_bag_dir, bag_file)
 
         with rosbag.Bag(bag_path, "r") as bag:
-
             for topic, msg, _ in bag.read_messages(topics=[args.image_topic]):
 
-
                 img_name = "frame_{}.jpg".format(generate_uuid1_name())
-
                 extraction_path = os.path.join(args.output_dir, img_name)
-
-                bridge = CvBridge()
 
                 cv_img = bridge.compressed_imgmsg_to_cv2(msg, desired_encoding='passthrough')
                 cv2.imwrite(extraction_path, cv_img)
+                logger.info('Extracted image {} to {}'.format(img_name, extraction_path))
 
 
 if __name__ == '__main__':
-    main()
+    logger = set_logger()
+    main(logger)
+    logger.info("Extraction of all bags complete with success")
